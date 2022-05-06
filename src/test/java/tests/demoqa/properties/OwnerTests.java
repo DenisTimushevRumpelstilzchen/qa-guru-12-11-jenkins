84c509f708ee0029d28bbdd5026f414db9917685
tdv14011984@gmail.com <Parol2022!-forGIT>
2022-05-06 12:57:16 +0300

    add Tests

--- src/test/java/tests/demoqa/properties/OwnerTests.java
+++ src/test/java/tests/demoqa/properties/OwnerTests.java
@@ -1,4 +1,4 @@
-package tests;
+package tests.demoqa.properties;
 
 import config.CredentialsConfig;
 import org.aeonbits.owner.ConfigFactory;
--- src/test/java/tests/demoqa/properties/SystemPropertiesTests.java
+++ src/test/java/tests/demoqa/properties/SystemPropertiesTests.java
@@ -1,4 +1,4 @@
-package tests;
+package tests.demoqa.properties;
 
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
